0b4b5b00ee2745e02ace76227547ebdd984dab3f
Chris Anderton <chrisanderton@users.noreply.github.com>
2022-09-02 01:13:34 +0100

    [wip] update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,28 +10,32 @@ USER=root
 
 # /var/spool/cron/crontabs/ is symlink of /etc/crontabs/
 CRONTAB="/var/spool/cron/crontabs/$USER"
-$PERIOD="/etc/periodic"
+PERIODS="/etc/periodic"
 
 cat <<EOF > $CRONTAB
-*/1	*	*	*	*	run-parts /etc/periodic/1min
-*/5	*	*	*	*	run-parts /etc/periodic/5min
-*/10	*	*	*	*	run-parts /etc/periodic/10min
-*/15	*	*	*	*	run-parts /etc/periodic/15min
-0	*	*	*	*	run-parts /etc/periodic/hourly
-0	2	*	*	*	run-parts /etc/periodic/daily
-0	3	*	*	6	run-parts /etc/periodic/weekly
-0	5	1	*	*	run-parts /etc/periodic/monthly
+*/1 * * * * run-parts /etc/periodic/1min
+*/5 * * * * run-parts /etc/periodic/5min
+*/10 * * * * run-parts /etc/periodic/10min
+*/15 * * * * run-parts /etc/periodic/15min
+0 * * * * run-parts /etc/periodic/hourly
+0 2 * * * run-parts /etc/periodic/daily
+0 3 * * 6 run-parts /etc/periodic/weekly
+0 5 1 * * run-parts /etc/periodic/monthly
 EOF
 
 # if cron is provided, override the default crontab
 if [ -n "$CRON" ]; then
-  echo -e "$CRON" > "$CRONTAB"
+  if [ "$APPEND" -eq "true" ]; then
+    echo -e "$CRON" >> "$CRONTAB"
+  else
+    echo -e "$CRON" > "$CRONTAB"
+  fi
 fi
 
 chown "$USER:$USER" "$CRONTAB"
 chmod 600 "$CRONTAB"
 
-mkdir -p $PERIOD/1min $PERIOD/5min $PERIOD/10min
-chown -R "$USER:$USER" $PERIOD
+mkdir -p $PERIODS/1min $PERIODS/5min $PERIODS/10min
+chown -R "$USER:$USER" $PERIODS
 
 exec "crond -f" "$@"
